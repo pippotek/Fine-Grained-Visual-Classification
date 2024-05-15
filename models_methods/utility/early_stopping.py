e78f80488b2f5245fe2ddd7ba80e8ea2f84e0d8a
Giuseppe Curci <giuseppecurci49@gmail.com>
2024-05-16 00:28:20 +0200

    testing SAM because slow convergence

--- models_methods/utility/early_stopping.py
+++ models_methods/utility/early_stopping.py
@@ -2,20 +2,21 @@ import torch
 import numpy as np
 
 class EarlyStopping:
-    def __init__(self, patience=5, delta=1e-3):
+    def __init__(self, path, patience=5, delta=1e-3):
         self.patience = patience
         self.delta = delta
         self.best_score = None
         self.early_stop = False
         self.counter = 0
-        self.best_loss = np.Inf
+        self.best_loss = 99999999
+        self.path = path
 
-    def __call__(self, val_loss, model, path):
+    def __call__(self, val_loss, model):
         score = -val_loss
 
         if self.best_score is None:
             self.best_score = score
-            self.save_checkpoint(val_loss, model, path)
+            self.save_checkpoint(val_loss, model)
         elif score < self.best_score + self.delta:
             self.counter += 1
             print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
@@ -23,10 +24,10 @@ class EarlyStopping:
                 self.early_stop = True
         else:
             self.best_score = score
-            self.save_checkpoint(val_loss, model, path)
+            self.save_checkpoint(val_loss, model)
             self.counter = 0
 
-    def save_checkpoint(self, val_loss, model, path):
+    def save_checkpoint(self, val_loss, model):
         '''Saves model when validation loss decreases.'''
         self.best_loss = val_loss
-        torch.save(model.state_dict(), path)
+        torch.save(model.state_dict(), self.path)
